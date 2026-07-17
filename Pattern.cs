8ee198534c12a78043d4c3d62b8a3257e482a5b4
C95Newman <133158289+C95Newman@users.noreply.github.com>
2026-07-17 11:09:43 -0400

    Adding Pattern Object

---
+++ Pattern.cs
@@ -0,0 +1,30 @@
+﻿using CrochetPatternBackend;
+
+namespace CrochetPatternBackend
+{
+    public enum PatternCategory
+    {
+        Apparel_General = 0,
+        Apparel_Shirt,
+        Apparel_Bottoms,
+        Toy_Stuffy,
+        Toy_Other
+
+    };
+    public class Pattern
+    {
+        public List<String> steps = new List<String>();
+
+        public string patternName = "";
+
+        public int yarnWeight = 4;
+
+        public PatternCategory category;
+        public static string getJSON(Pattern pattern)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(pattern);
+        }
+
+    }
+    
+}
